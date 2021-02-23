25dd937830e14fafb25bac502b01d5cde1796b63
Justin Oberg <oberg218@morris.umn.edu>
2021-02-23 13:45:35 -0600

    Comment out constructor in todos service
    commented out constructor in todos service until we can
    get the rest of the code implemented.

--- client/src/app/todos/todos.service.ts
+++ client/src/app/todos/todos.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { getMatTooltipInvalidPositionError } from '@angular/material/tooltip';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+  readonly todoUrl: string = environment.apiUrl + 'todos';
 
-  constructor(private httpClient: HttpClient) { }
+  //constructor(private httpClient: HttpClient) { }
 }
 
