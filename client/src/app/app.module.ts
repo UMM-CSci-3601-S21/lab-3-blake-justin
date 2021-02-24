8b2e5ae3954bda8807cc50e6321ad6ce9e476fa4
Justin Oberg <oberg218@morris.umn.edu>
2021-02-23 20:33:01 -0600

    Generate todo-list component
    Used command line to have angular generate a todo-list component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodosService } from './todos/todos.service';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -61,6 +62,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     TodosComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
